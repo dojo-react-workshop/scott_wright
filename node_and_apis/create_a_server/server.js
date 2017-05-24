08c87c3544041b75ab355438d3288c438c1ae6ab
WRIGHT, SCOTT <scottwright@northwesternmutual.com>
2017-05-24 13:30:37 -0500

    landing_page

--- node_and_apis/create_a_server/server.js
+++ node_and_apis/create_a_server/server.js
@@ -4,13 +4,16 @@ var fs = require('fs')
 var server = http.createServer(function(request, response) {
   return new Promise((good, bad)=>{
     fs.readFile('index.html','utf8', (err, data) => {
+      if (err) {
+        bad(err)
+      }
       good(data)  
     })
   }).then((data)=>{
     response.write(data)
     response.end()
   }).catch((err)=>{
-    throw new Error(err)
+    throw new Error(err.message)
   })
 })
   
---
+++ node_and_apis/landing_page/server.js
@@ -0,0 +1,42 @@
+// localhost:6789/ -- This route should serve a view file called index.html and display a greeting.
+// localhost:6789/ninjas -- This route should serve a view file called ninjas.html and display information about ninjas.
+// localhost:6789/dojos/new -- This route should serve a view file called dojos.html and have a form (don't worry about where the form should be sent to).
+
+
+var http = require('http');
+var fs = require('fs')
+
+var server = http.createServer(function(request, response) {
+    switch (request.url) {
+        case '/' :
+            loadFile('index.html').then((data)=>response.end(data))
+        break
+        case '/ninjas' :
+            loadFile('ninjas.html').then((data)=>response.end(data))
+        break
+        case '/dojos/new' :
+        loadFile('dojos.html').then((data)=>response.end(data))
+        case '/sync' :
+        loadFile('scott.html').then((data)=>response.end(data))
+        break
+        default :
+            response.end('no page here!!!!')
+    }    
+})
+
+server.listen(6789);
+
+function loadFile(file){
+    if (file == "scott.html"){
+        return Promise.resolve("sync data")
+    } else {
+    return new Promise((good, bad)=>{
+        fs.readFile(file,'utf8', (err, data) => {
+            if (err) {
+                bad(err)
+            }
+            good(data)  
+            })
+        })
+    }
+}
