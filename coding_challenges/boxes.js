d161407f99a1518da2636a4450ac379643880956
WRIGHT, SCOTT <scottwright@northwesternmutual.com>
2017-06-20 14:57:37 -0500

    did some more stuff

--- coding_challenges/boxes.js
+++ coding_challenges/boxes.js
@@ -1,5 +1,68 @@
 function boxes(o1, o2){
 
+    function boxes(o1, o2){
+
+    let newLX=Math.max(o1.x,o2.x)
+    let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
+    let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
+    let newBY=Math.max(o1.y,o2.y)
+
+    let newW=newRX-newLX
+    let newH=newTY-newBY
+
+    if (newW <=0 || newH <=0){
+        return{
+            x:null,
+            y:null,
+            w:null,
+            h:null     
+        }
+    }
+    return{
+        x:newLX,
+        y:newBY,
+        w:newW,
+        h:newH
+    }
+
+}
+
+//base test
+console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
+
+//touching not overlap
+function boxes(o1, o2){
+
+    let newLX=Math.max(o1.x,o2.x)
+    let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
+    let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
+    let newBY=Math.max(o1.y,o2.y)
+
+    let newW=newRX-newLX
+    let newH=newTY-newBY
+
+    if (newW <=0 || newH <=0){
+        return{
+            x:null,
+            y:null,
+            w:null,
+            h:null     
+        }
+    }
+    return{
+        x:newLX,
+        y:newBY,
+        w:newW,
+        h:newH
+    }
+
+}
+
+//base test
+console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
+
+//touching not overlap
+
     let newLX=Math.max(o1.x,o2.x)
     let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
     let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
@@ -30,3 +93,124 @@ console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
 
 //touching not overlap
 console.log(boxes({x:-1,y:-2,w:3,h:4},{x:-2,y:-4,w:2,h:3}))
+function boxes(o1, o2){
+
+    let newLX=Math.max(o1.x,o2.x)
+    let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
+    let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
+    let newBY=Math.max(o1.y,o2.y)
+
+    let newW=newRX-newLX
+    let newH=newTY-newBY
+
+    if (newW <=0 || newH <=0){
+        return{
+            x:null,
+            y:null,
+            w:null,
+            h:null     
+        }
+    }
+    return{
+        x:newLX,
+        y:newBY,
+        w:newW,
+        h:newH
+    }
+
+}
+
+//base test
+console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
+
+//touching not overlapfunction boxes(o1, o2){
+
+    let newLX=Math.max(o1.x,o2.x)
+    let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
+    let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
+    let newBY=Math.max(o1.y,o2.y)
+
+    let newW=newRX-newLX
+    let newH=newTY-newBY
+
+    if (newW <=0 || newH <=0){
+        return{
+            x:null,
+            y:null,
+            w:null,
+            h:null     
+        }
+    }
+    return{
+        x:newLX,
+        y:newBY,
+        w:newW,
+        h:newH
+    }
+
+}
+
+//base test
+console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
+
+//touching not overlapfunction boxes(o1, o2){
+
+    let newLX=Math.max(o1.x,o2.x)
+    let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
+    let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
+    let newBY=Math.max(o1.y,o2.y)
+
+    let newW=newRX-newLX
+    let newH=newTY-newBY
+
+    if (newW <=0 || newH <=0){
+        return{
+            x:null,
+            y:null,
+            w:null,
+            h:null     
+        }
+    }
+    return{
+        x:newLX,
+        y:newBY,
+        w:newW,
+        h:newH
+    }
+
+}
+
+//base test
+console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
+
+//touching not overlapfunction boxes(o1, o2){
+
+    let newLX=Math.max(o1.x,o2.x)
+    let newRX=Math.min(o1.x+o1.w,o2.x+o2.w)
+    let newTY=Math.min(o1.y+o1.h,o2.y+o2.h)
+    let newBY=Math.max(o1.y,o2.y)
+
+    let newW=newRX-newLX
+    let newH=newTY-newBY
+
+    if (newW <=0 || newH <=0){
+        return{
+            x:null,
+            y:null,
+            w:null,
+            h:null     
+        }
+    }
+    return{
+        x:newLX,
+        y:newBY,
+        w:newW,
+        h:newH
+    }
+
+}
+
+//base test
+console.log(boxes({x:1,y:2,w:3,h:4},{x:2,y:4,w:1,h:3}))
+
+//touching not overlap
